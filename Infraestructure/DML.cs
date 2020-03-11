fdbb62efdf26f9c31b1ea22326ebab1f72d05027
Jonathan Steven Soto Orjuela <sotojony32@gmail.com>
2020-03-11 17:28:50 -0500

    Se añade el proyecto ceramicas

---
+++ Infraestructure/DML.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure
+{
+    public interface DML<T>
+    {
+        Task<T> Add(T element);
+        Task<T> Remove(T element);
+        List<T> Get(T element);
+    }
+}
